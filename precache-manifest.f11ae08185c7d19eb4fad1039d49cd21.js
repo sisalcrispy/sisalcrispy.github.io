3c17cd523c303509889f0021891c92433a215bca
Ludovico <ludovico.tamigio@crispybacon.it>
2020-03-31 15:09:18 +0200

    first commit

---
+++ precache-manifest.f11ae08185c7d19eb4fad1039d49cd21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493b058def4e6abe3e590d7135fbe08f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1d072126286003c40ef0",
+    "url": "./static/css/2.cd374407.chunk.css"
+  },
+  {
+    "revision": "240f049804a6ab037006",
+    "url": "./static/css/main.c3dfe1c3.chunk.css"
+  },
+  {
+    "revision": "1d072126286003c40ef0",
+    "url": "./static/js/2.2d23f997.chunk.js"
+  },
+  {
+    "revision": "ee9b20861703f370798eb6901e0554ff",
+    "url": "./static/js/2.2d23f997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240f049804a6ab037006",
+    "url": "./static/js/main.bfa1e996.chunk.js"
+  },
+  {
+    "revision": "5def09dce36f6b9a6d6e",
+    "url": "./static/js/runtime-main.e8508e20.js"
+  },
+  {
+    "revision": "30c9b0e562667e8a1d1573824affb335",
+    "url": "./static/media/logo.30c9b0e5.svg"
+  }
+]);
